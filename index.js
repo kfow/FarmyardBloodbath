0857c0329b7b383b071fbadc1476d4d5ab137827
greg12thomson@Gmail.com <GregisGinger2@googlemail.com>
2016-10-14 23:00:17 +0100

    Started to add game

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var util = require('util')
+var http = require('http')
+var path = require('path')
+//var ecstatic = require('ecstatic')
+var io = require('socket.io')
 var express = require('express');
+
+var Player = require('./lib/Player')
 
 var app = express();
 
@@ -9,13 +16,113 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-// More imports here
-app.set('port', process.env.PORT || 3000);
+// Set port
+var port = process.env.PORT || 3000
+app.set('port', port);
 
 // set static to /public
 app.use(express.static(__dirname + '/public'));
 
-// -----------------------------------------------------------------------------
+/* ---------------------------------------------------------------------------
+ * GAME VARIABLES
+------------------------------------------------------------------------------ */
+var socket;	// Socket controller
+var players;	// Array of connected players
+
+/* ---------------------------------------------------------------------------
+ * GAME INITIALISATION
+------------------------------------------------------------------------------ */
+// Create and start the http server
+app.listen(app.get('port'), function(err) {
+  if (err) {
+    throw err
+  }
+  console.log('Server started press Ctrl-C to terminate');
+  init()
+
+})
+
+function init () {
+  // Create an empty array to store players
+  players = []
+  // Attach Socket.IO to server
+  socket = io.listen(app.get('server'))
+  // Start listening for events
+  setEventHandlers()
+}
+
+/* ---------------------------------------------------------------------------
+ * GAME EVENT HANDLERS
+------------------------------------------------------------------------------ */
+
+var setEventHandlers = function () {
+  // Socket.IO
+  socket.sockets.on('connection', onSocketConnection);
+}
+
+// New socket connection
+function onSocketConnection (client) {
+  util.log('New player has connected: ' + client.id);
+  // Listen for client disconnected
+  client.on('disconnect', onClientDisconnect);
+  // Listen for new player message
+  client.on('new player', onNewPlayer);
+  // Listen for move player message
+  client.on('move player', onMovePlayer);
+}
+
+// Socket client has disconnected
+function onClientDisconnect () {
+  util.log('Player has disconnected: ' + this.id)
+  var removePlayer = playerById(this.id)
+  // Player not found
+  if (!removePlayer) {
+    util.log('Player not found: ' + this.id)
+    return
+  }
+  // Remove player from players array
+  players.splice(players.indexOf(removePlayer), 1)
+  // Broadcast removed player to connected socket clients
+  this.broadcast.emit('remove player', {id: this.id})
+}
+
+// New player has joined
+function onNewPlayer (data) {
+  // Create a new player
+  var newPlayer = new Player(data.x, data.y, data.angle)
+  newPlayer.id = this.id
+  // Broadcast new player to connected socket clients
+  this.broadcast.emit('new player', {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY(), angle: newPlayer.getAngle()})
+  // Send existing players to the new player
+  var i, existingPlayer
+  for (i = 0; i < players.length; i++) {
+    existingPlayer = players[i]
+    this.emit('new player', {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY(), angle: existingPlayer.getAngle()})
+  }
+  // Add new player to the players array
+  players.push(newPlayer)
+}
+
+// Player has moved
+function onMovePlayer (data) {
+  // Find player in array
+  var movePlayer = playerById(this.id)
+
+  // Player not found
+  if (!movePlayer) {
+    util.log('Player not found: ' + this.id)
+    return
+  }
+
+  // Update player position
+  movePlayer.setX(data.x)
+  movePlayer.setY(data.y)
+  movePlayer.setAngle(data.angle)
+
+  // Broadcast updated position to connected socket clients
+  this.broadcast.emit('move player', {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY(), angle: movePlayer.getAngle()})
+}
+
 // base directory
 app.get('/', function(req, res){
   res.render('home');
@@ -25,11 +132,6 @@ app.use(function(req, res, next){
   console.log("Looking for URL " + req.url);
   next();
 });
-
-//app.use(function(err, req, res, next){
-//  console.log('Error: ' + err.message);
-//  next();
-//})
 
 app.get('/about', function(req, res){
   res.render('about');
@@ -45,8 +147,17 @@ app.use(function(req, res){
   res.render('404');
 })
 
+/* ---------------------------------------------------------------------------
+ * GAME HELPER FUNCTIONS
+------------------------------------------------------------------------------ */
+// Find player by ID
+function playerById (id) {
+  var i
+  for (i = 0; i < players.length; i++) {
+    if (players[i].id === id) {
+      return players[i]
+    }
+  }
 
-// -----------------------------------------------------------------------------
-app.listen(app.get('port'), function(){
-    console.log('Express started press Ctrl-C to terminate')
-});
+  return false
+}
